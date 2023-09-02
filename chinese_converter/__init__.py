2e792a8f5e5623a4adc9ca5c4ee4a8833500e265
Zachary Juang <zachary822@me.com>
2023-09-02 18:58:21 -0400

    modernize package

--- chinese_converter/__init__.py
+++ chinese_converter/__init__.py
@@ -1,20 +1,24 @@
 import json
-import os
 from collections import Counter, defaultdict, deque
+from importlib import resources
 from operator import itemgetter
-from typing import Any, Deque, Dict, List, Optional, Tuple
+from typing import Any
 
-__all__ = ['to_simplified', 'to_traditional']
+__all__ = ["to_simplified", "to_traditional"]
 
-BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+PKG = resources.files("chinese_converter")
 
-with open(os.path.join(BASE_PATH, 'traditional.txt')) as f, open(os.path.join(BASE_PATH, 'simplified.txt')) as g:
+with resources.as_file(PKG / "traditional.txt") as p, p.open(
+    encoding="utf-8"
+) as f, resources.as_file(PKG / "simplified.txt") as q, q.open(encoding="utf-8") as g:
     traditional, simplified = f.read().strip(), g.read().strip()
 
-with open(os.path.join(BASE_PATH, 'bigram.json')) as f, open(os.path.join(BASE_PATH, 'monogram.json')) as g:
+with resources.as_file(PKG / "bigram.json") as p, p.open(
+    encoding="utf-8"
+) as f, resources.as_file(PKG / "monogram.json") as q, q.open(encoding="utf-8") as g:
     bigrams, monograms = Counter(json.load(f)), Counter(json.load(g))
 
-simp_to_trad: Dict[str, List[str]] = defaultdict(list)
+simp_to_trad: dict[str, list[str]] = defaultdict(list)
 trad_to_simp = str.maketrans(traditional, simplified)
 
 for s, t in zip(simplified, traditional):
@@ -23,15 +27,17 @@ for s, t in zip(simplified, traditional):
 simp_to_trad = dict(simp_to_trad)
 
 
-def default_getter(l, i, default: Optional[Any] = None):
+def default_getter(l, i, default: Any | None = None):
     try:
         return l[i]
     except IndexError:
         return default
 
 
-def most_common_word(choices: List[str], prev_word: Optional[str] = None, next_word: Optional[str] = None) -> str:
-    bigram_freqs: Deque[Tuple[str, int]] = deque()
+def most_common_word(
+    choices: list[str], prev_word: str | None = None, next_word: str | None = None
+) -> str:
+    bigram_freqs: deque[tuple[str, int]] = deque()
 
     if prev_word is not None:
         bigram_freqs.extend((c, bigrams[prev_word + c]) for c in choices)
@@ -57,20 +63,24 @@ def to_traditional(text: str) -> str:
     :param text:
     :return:
     """
-    result: Deque[str] = deque()
+    result: deque[str] = deque()
 
     for i, s in enumerate(text):
         try:
             choices = simp_to_trad[s]
 
             if len(choices) > 1:
-                result.append(most_common_word(choices, default_getter(result, -1), default_getter(text, i + 1)))
+                result.append(
+                    most_common_word(
+                        choices, default_getter(result, -1), default_getter(text, i + 1)
+                    )
+                )
             else:
                 result.append(choices[0])
         except KeyError:
             result.append(s)
 
-    return ''.join(result)
+    return "".join(result)
 
 
 def to_simplified(text: str) -> str:
