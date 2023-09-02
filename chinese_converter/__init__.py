cea1f65300930f4cb2da3d28d76e1aa3867c0e2e
Zachary Juang <zachary822@me.com>
2023-09-02 19:05:07 -0400

    run pre-commit

--- chinese_converter/__init__.py
+++ chinese_converter/__init__.py
@@ -8,14 +8,14 @@ __all__ = ["to_simplified", "to_traditional"]
 
 PKG = resources.files("chinese_converter")
 
-with resources.as_file(PKG / "traditional.txt") as p, p.open(
-    encoding="utf-8"
-) as f, resources.as_file(PKG / "simplified.txt") as q, q.open(encoding="utf-8") as g:
+with resources.as_file(PKG / "traditional.txt") as p, p.open(encoding="utf-8") as f, resources.as_file(
+    PKG / "simplified.txt"
+) as q, q.open(encoding="utf-8") as g:
     traditional, simplified = f.read().strip(), g.read().strip()
 
-with resources.as_file(PKG / "bigram.json") as p, p.open(
-    encoding="utf-8"
-) as f, resources.as_file(PKG / "monogram.json") as q, q.open(encoding="utf-8") as g:
+with resources.as_file(PKG / "bigram.json") as p, p.open(encoding="utf-8") as f, resources.as_file(
+    PKG / "monogram.json"
+) as q, q.open(encoding="utf-8") as g:
     bigrams, monograms = Counter(json.load(f)), Counter(json.load(g))
 
 simp_to_trad: dict[str, list[str]] = defaultdict(list)
@@ -27,16 +27,14 @@ for s, t in zip(simplified, traditional):
 simp_to_trad = dict(simp_to_trad)
 
 
-def default_getter(l, i, default: Any | None = None):
+def default_getter(lst, i, default: Any | None = None):
     try:
-        return l[i]
+        return lst[i]
     except IndexError:
         return default
 
 
-def most_common_word(
-    choices: list[str], prev_word: str | None = None, next_word: str | None = None
-) -> str:
+def most_common_word(choices: list[str], prev_word: str | None = None, next_word: str | None = None) -> str:
     bigram_freqs: deque[tuple[str, int]] = deque()
 
     if prev_word is not None:
@@ -70,11 +68,7 @@ def to_traditional(text: str) -> str:
             choices = simp_to_trad[s]
 
             if len(choices) > 1:
-                result.append(
-                    most_common_word(
-                        choices, default_getter(result, -1), default_getter(text, i + 1)
-                    )
-                )
+                result.append(most_common_word(choices, default_getter(result, -1), default_getter(text, i + 1)))
             else:
                 result.append(choices[0])
         except KeyError:
